9f55d5b2b6065c250b60871fec82313e1f0afb37
faturalkhair <105841107021@student.unismuh.ac.id>
2024-09-24 10:52:51 +0800

    1-3

--- Acumalaka/p1.py
+++ Acumalaka/p1.py
@@ -1,1 +1,29 @@
 # mari kita coba
+print ("Hello World!")
+
+
+name = "fath"
+age = 22
+heigh = 5.8
+fruits = ["Kelapa", "Mangga", "Rappo Ballo'", "Caramele'", "Lobe-Lobe"]
+person = {"name": "akki", "age": 25}
+
+x = 1
+if x > 5:
+    print("x lebih besar daripada 5")
+elif x == 5:
+    print("x sama dengan 5")
+else:
+    print("x lebih kecil daripada 5")
+# ...
+
+# Loop
+
+fruits = ["Kelapa", "Mangga", "Rappo Ballo'", "Caramele'", "Lobe-Lobe"]
+for fruit in fruits:
+    print("saya ku suka " + fruit)
+
+count = 0
+while count < 10:
+    print("perulangan: " + str(count))
+    count += 1
