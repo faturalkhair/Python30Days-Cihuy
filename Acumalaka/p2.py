7a79f2a81520020d123ab3dfc457874480c09a64
faturalkhair <105841107021@student.unismuh.ac.id>
2024-09-25 09:32:18 +0800

    apakah ini ku bikin?

---
+++ Acumalaka/p2.py
@@ -0,0 +1,19 @@
+print("adakah")
+print("========================")
+
+umur = 20
+if umur >= 20:
+    print("hahaha tua")
+else:
+    print("masih muda brow")
+
+
+nilai = 100
+if nilai >= 90:
+    print("A")
+elif nilai >= 80:
+    print("B")
+elif nilai >= 70:
+    print("C")
+else:
+    print("gagal jink")
